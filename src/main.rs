552b0dfbe71187df384b9fdc8d98a1d802053e8e
kraftwerk28 <kefirchik3@gmail.com>
2020-11-02 12:32:12 +0200

    Fix: version output

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,25 @@
 mod game;
 mod utils;
 
-use clap::App;
+use clap::{App, AppSettings, Arg};
 use game::*;
 use utils::EndState::*;
 
 fn main() {
-    App::new(env!("CARGO_PKG_NAME"))
-        .version(env!("CARGO_PKG_VERSION"))
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .setting(AppSettings::DisableVersion)
+        .arg(
+            Arg::with_name("version")
+                .short("v")
+                .long("version")
+                .help("Show version"),
+        )
         .get_matches();
+
+    if matches.is_present("version") {
+        println!(env!("CARGO_PKG_VERSION"));
+        return;
+    }
 
     let mut game = GameState::new("bot.log");
     game.run();
